0e13e4f3fbec5e13216ad18fe6b60805fa27582d
Valentino Dola <dolavalentino@gmail.com>
2022-05-15 17:36:54 +0000

    Done with Splashscreen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from './Navigation';
+import RNBootSplash from 'react-native-bootsplash';
 
 const App = () => {
-  return <Navigation />;
+  useEffect(() => {
+    RNBootSplash.hide({fade: true})
+      .then(r => console.log(r, 'Bootsplash has been hidden successfully'))
+      .catch(err => console.error(err));
+  }, []);
+  return <Navigation/>;
 };
 
 export default App;
