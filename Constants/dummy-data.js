c0c63d2952658d155a5c291ef33d795fcf3d99bc
Valentino Dola <dolavalentino@gmail.com>
2022-06-22 21:49:53 +0000

    Finish the detail screen

--- Constants/dummy-data.js
+++ Constants/dummy-data.js
@@ -1,3 +1,7 @@
+import {Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
 const events = [
   {
     id: 1,
@@ -6,7 +10,8 @@ const events = [
     startTime: '2020/01/21 09:10 PM',
     title: 'Festivale des couleurs',
     location: '8010, Adamavo Market, Lome',
-    description: 'Description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
     price: 5000,
   },
   // {
@@ -16,7 +21,7 @@ const events = [
   //   startTime: '23/03/2022 13h00',
   //   title: 'UX Design',
   //   location: '8010, Adamavo Market, Lome',
-  //   description: 'Description',
+  //   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
   //   price: 3500,
   // },
   {
@@ -26,8 +31,9 @@ const events = [
     startTime: '2020/02/21 09:10 PM',
     title: 'UI/UX',
     location: '8010, Adamavo Market, Lome',
-    description: 'Description',
-    price: 'Free',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    price: 0,
   },
   {
     id: 4,
@@ -36,7 +42,8 @@ const events = [
     startTime: '2020/04/21 09:10 PM',
     title: 'Festivale des couleurs',
     location: '8010, Adamavo Market, Lome',
-    description: 'Description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
     price: 1000,
   },
   {
@@ -46,8 +53,9 @@ const events = [
     startTime: '2020/06/21 09:10 PM',
     title: 'Festivale des couleurs',
     location: '8010, Adamavo Market, Lome',
-    description: 'Description',
-    price: 'Free',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    price: 3000,
   },
   {
     id: 6,
@@ -56,8 +64,9 @@ const events = [
     startTime: '2020/08/21 09:10 PM',
     title: 'Festivale des couleurs',
     location: '8010, Adamavo Market, Lome',
-    description: 'Description',
-    price: 'Free',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    price: 10000,
   },
   {
     id: 7,
@@ -66,14 +75,204 @@ const events = [
     startTime: '2020/10/21 09:10 PM',
     title: 'Festivale des couleurs',
     location: '8010, Adamavo Market, Lome',
-    description: 'Description',
-    price: 'Free',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    price: 50000,
   },
 ];
 
-const region = {
-  latitude: 6.160584,
-  longitude: 1.31352,
+const Region = {
+  latitude: 37.58817,
+  longitude: -122.4903973,
+  latitudeDelta: 0.005,
+  longitudeDelta: 0.005 * (width / height),
 };
 
-export {events, region};
+const MapStyle = [
+  {
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#212121',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#212121',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.country',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#9e9e9e',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.locality',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#bdbdbd',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#181818',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#616161',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#1b1b1b',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#2c2c2c',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#8a8a8a',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#373737',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#3c3c3c',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#4e4e4e',
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#616161',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#757575',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#000000',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#3d3d3d',
+      },
+    ],
+  },
+];
+
+export {events, Region, MapStyle};
--- Screens/Détail.tsx
+++ Screens/Détail.tsx
@@ -4,13 +4,13 @@
  *
  * TODO:
  * [] Build the header image background section
- *    [] Rendering the image background sub section (ImageBackground)
- *    [] Rendering the header sub section
- *    [] Rendering the footer sub section (LinearGradient)
- * [] Build the buttons group section
- * [] Build the description section
+ *    [X] Rendering the image background sub section (ImageBackground)
+ *    [X] Rendering the header sub section
+ *    [X] Rendering the footer sub section
+ * [X] Build the buttons group section
+ * [X] Build the description section
  * [] Build the location section (google map in dark mode)
- * [] Build the fixed bottom bar
+ * [X] Build the fixed bottom bar
  */
 
 import React, {useState, useEffect} from 'react';
@@ -23,12 +23,13 @@ import {
   TouchableOpacity,
   StyleSheet,
   Dimensions,
-  LinearGradient,
+  Image,
 } from 'react-native';
 import moment from 'moment';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {events} from '../Constants/dummy-data';
+import {Region, MapStyle} from '../Constants/dummy-data';
 import {theme} from '../Constants/index';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const {width, height} = Dimensions.get('window');
 
@@ -40,118 +41,338 @@ const Detail = ({navigation, route}: any) => {
     setSelectedEvent(selectedEvent);
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        contentContainerStyle={{flexGrow: 1, backgroundColor: '#F6F6F7'}}
-        style={{backgroundColor: '#F6F6F7'}}>
-        {/* ImageBackground */}
-        <ImageBackground
-          source={selectedEvent?.image}
-          style={{width: '100%', height: height / 2}}>
-          {/* Image Header */}
-          <View style={{flex: 1}}>
+  const ImageBackgroundComponent = () => {
+    return (
+      <ImageBackground
+        source={selectedEvent?.image}
+        style={{width: '100%', height: height / 2.2}}>
+        {/* Image Header */}
+        <View style={{flex: 1}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginTop: Platform.OS === 'ios' ? 30 : 20,
+              marginHorizontal: 15,
+            }}>
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{
+                backgroundColor: theme.colors.grey,
+                padding: 5,
+                borderRadius: 10,
+                opacity: 0.7,
+              }}>
+              <Icon
+                name="ios-chevron-back-outline"
+                size={24}
+                color={theme.colors.black}
+              />
+            </TouchableOpacity>
             <View
               style={{
                 flexDirection: 'row',
-                justifyContent: 'space-between',
-                marginTop: Platform.OS === 'ios' ? 30 : 20,
-                marginHorizontal: 15,
+                justifyContent: 'space-around',
+                backgroundColor: theme.colors.grey,
+                alignItems: 'center',
+                paddingHorizontal: 10,
+                borderRadius: 5,
+                opacity: 0.7,
               }}>
-              <TouchableOpacity
-                onPress={() => navigation.goBack()}
-                style={{
-                  backgroundColor: theme.colors.grey,
-                  padding: 5,
-                  borderRadius: 10,
-                  opacity: 0.7,
-                }}>
+              <TouchableOpacity onPress={() => console.log('You like it')}>
                 <Icon
-                  name="ios-chevron-back-outline"
+                  name="ios-heart-outline"
                   size={24}
                   color={theme.colors.black}
                 />
               </TouchableOpacity>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-around',
-                  backgroundColor: theme.colors.grey,
-                  alignItems: 'center',
-                  paddingHorizontal: 10,
-                  borderRadius: 5,
-                  opacity: 0.7,
-                }}>
-                <TouchableOpacity onPress={() => console.log('You like it')}>
-                  <Icon
-                    name="ios-heart-outline"
-                    size={24}
-                    color={theme.colors.black}
-                  />
-                </TouchableOpacity>
-                <View style={{width: 15}} />
-                <TouchableOpacity onPress={() => console.log('Share it')}>
-                  <Icon
-                    name="ios-share-outline"
-                    size={24}
-                    color={theme.colors.black}
-                  />
-                </TouchableOpacity>
-              </View>
+              <View style={{width: 15}} />
+              <TouchableOpacity onPress={() => console.log('Share it')}>
+                <Icon
+                  name="ios-share-outline"
+                  size={24}
+                  color={theme.colors.black}
+                />
+              </TouchableOpacity>
             </View>
           </View>
-          {/* Image footer */}
+        </View>
+        {/* Image footer */}
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'flex-end',
+          }}>
           <View
             style={{
-              flex: 1,
-              justifyContent: 'flex-end',
-              position: 'relative',
-              zIndex: -1,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginLeft: 15,
+              marginBottom: 30,
             }}>
-            {/* <LinearGradient
-              color={['transparent', '#000']}
-              start={{x: 0, y: 1}}
-              end={{x: 0, y: 1}}
-              style={{width, height: height / 2}}></LinearGradient> */}
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                margin: '0px 30px',
-              }}>
-              <View>
-                <Text>{selectedEvent?.type}</Text>
-                <Text>{selectedEvent?.title}</Text>
-                <Text>{selectedEvent?.startTime}</Text>
-              </View>
-            </View>
-            {/* <View style={{marginLeft: 10, marginBottom: 15}}>
+            <View>
               <Text
                 style={{
                   textTransform: 'uppercase',
                   fontFamily: 'Nunito-SemiBold',
                   opacity: 0.6,
-                  marginVertical: 10,
+                  letterSpacing: 2,
                   fontSize: theme.sizes.h5,
                   color: theme.colors.white,
                 }}>
-                {' '}
-                {selectedEvent?.type}{' '}
+                {selectedEvent?.type}
               </Text>
               <Text
                 style={{
                   fontFamily: 'Nunito-SemiBold',
-                  fontSize: theme.sizes.h4,
+
+                  fontSize: theme.sizes.h3,
                   color: theme.colors.white,
                 }}>
-                {' '}
-                {selectedEvent?.title}{' '}
+                {selectedEvent?.title}
               </Text>
-            </View> */}
+              <Text
+                style={{
+                  fontFamily: 'Nunito-SemiBold',
+                  opacity: 0.6,
+                  letterSpacing: 1.5,
+                  fontSize: theme.sizes.h5,
+                  color: theme.colors.white,
+                }}>
+                Départ. {moment(selectedEvent?.startTime).format('HH:mm')}
+              </Text>
+            </View>
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                backgroundColor: theme.colors.white,
+                borderRadius: 5,
+                justifyContent: 'center',
+                alignItems: 'center',
+                opacity: 0.5,
+                marginRight: 15,
+              }}>
+              <Text
+                style={{
+                  color: theme.colors.black,
+                  fontFamily: 'Nunito-SemiBold',
+                }}>
+                {moment(selectedEvent?.startTime).format('MMM')}
+              </Text>
+              <Text
+                style={{
+                  color: theme.colors.black,
+                  fontFamily: 'Nunito-SemiBold',
+                  fontSize: theme.sizes.h3,
+                }}>
+                {moment(selectedEvent?.startTime).format('DD')}
+              </Text>
+            </View>
           </View>
-        </ImageBackground>
+        </View>
+      </ImageBackground>
+    );
+  };
+
+  const DescriptionButton = () => {
+    return (
+      <View
+        style={{
+          margin: 15,
+          flexDirection: 'row',
+        }}>
+        <TouchableOpacity
+          style={{
+            borderRadius: 5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: theme.colors.bluetiful,
+          }}>
+          <Text
+            style={{
+              color: theme.colors.white,
+              fontFamily: 'Nunito-SemiBold',
+              paddingVertical: 10,
+              paddingHorizontal: 15,
+              fontSize: theme.sizes.h7,
+            }}>
+            À propos de
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            borderRadius: 5,
+            marginHorizontal: 15,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: theme.colors.white,
+          }}>
+          <Text
+            style={{
+              color: theme.colors.black,
+              fontFamily: 'Nunito-SemiBold',
+              paddingVertical: 10,
+              paddingHorizontal: 15,
+              fontSize: theme.sizes.h7,
+            }}>
+            Participants
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  const DescriptionSection = () => {
+    return (
+      <View style={{marginHorizontal: 20}}>
+        <Text
+          style={{
+            color: theme.colors.black,
+            fontFamily: 'Nunito-SemiBold',
+            fontSize: theme.sizes.h7,
+          }}>
+          {selectedEvent?.description}
+        </Text>
+      </View>
+    );
+  };
+
+  const LocationSection = () => {
+    return (
+      <View style={{marginTop: 15, marginHorizontal: 20}}>
+        <Text
+          style={{
+            color: theme.colors.black,
+            fontFamily: 'Nunito-SemiBold',
+            fontSize: theme.sizes.h4,
+          }}>
+          Location
+        </Text>
+        <View style={{height: 250}}>
+          <MapView
+            provider={PROVIDER_GOOGLE}
+            style={{height: 250, borderRadius: 30, marginTop: 20}}
+            minZoomLevel={15}
+            region={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}></MapView>
+        </View>
+        <View style={{paddingBottom: 120}} />
+      </View>
+    );
+  };
+
+  const ButtomBarSection = () => {
+    return (
+      <View
+        style={{
+          height: 80,
+          width,
+          borderRadius: 10,
+          opacity: 0.9,
+          position: 'absolute',
+          backgroundColor: theme.colors.grey,
+          bottom: 0,
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginHorizontal: 20,
+          }}>
+          <View>
+            {selectedEvent?.price !== 0 && (
+              <Text
+                style={{
+                  color: theme.colors.black,
+                  fontFamily: 'Nunito-SemiBold',
+                  letterSpacing: 1,
+                  opacity: 0.8,
+                }}>
+                PRIX
+              </Text>
+            )}
+
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={{
+                  color: theme.colors.black,
+                  fontFamily: 'Nunito-SemiBold',
+                  fontSize: theme.sizes.h3,
+                }}>
+                {selectedEvent?.price === 0 ? 'Gratuit' : selectedEvent?.price}
+              </Text>
+              {selectedEvent?.price !== 0 && (
+                <Text
+                  style={{
+                    color: theme.colors.black,
+                    fontFamily: 'Nunito-SemiBold',
+                    fontSize: theme.sizes.h6,
+                  }}>
+                  /personne
+                </Text>
+              )}
+            </View>
+          </View>
+          {selectedEvent?.price !== 0 && (
+            <TouchableOpacity
+              style={{
+                flexDirection: 'row',
+                borderRadius: 5,
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                backgroundColor: theme.colors.bluetiful,
+              }}>
+              <Text
+                style={{
+                  color: theme.colors.white,
+                  fontFamily: 'Nunito-SemiBold',
+                  paddingVertical: 10,
+                  paddingHorizontal: 10,
+                  fontSize: theme.sizes.h6,
+                }}>
+                Acheter Billet
+              </Text>
+              <Image
+                source={require('../assets/images/splash/ticket.png')}
+                style={{width: 23, height: 23, marginRight: 10}}
+              />
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{flexGrow: 1, backgroundColor: '#F6F6F7'}}
+        style={{backgroundColor: '#F6F6F7'}}>
+        {/* ImageBackground */}
+        <ImageBackgroundComponent />
+        {/* Buttons group section */}
+        <DescriptionButton />
+        {/* Description section */}
+        <DescriptionSection />
+        {/* Location section */}
+        <LocationSection />
       </ScrollView>
+      {/* Buttom bar section */}
+      <ButtomBarSection />
     </View>
   );
 };
