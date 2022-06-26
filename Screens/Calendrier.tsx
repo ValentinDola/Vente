5a477ec7cfd401ba1c506ea5bdf159cbe385286e
Valentino Dola <dolavalentino@gmail.com>
2022-06-26 18:40:09 +0000

    Some changes

--- Screens/Calendrier.tsx
+++ Screens/Calendrier.tsx
@@ -3,50 +3,11 @@ import {StyleSheet, Text, View} from 'react-native';
 import {theme} from '../Constants/index';
 
 const Calendrier = () => (
-  <View style={styles.screen}>
+  <View style={{flex: 1, backgroundColor: '#F6F6F7'}}>
     <Text style={{color: theme.colors.blue, fontFamily: 'Nunito-SemiBold'}}>
       Calendrier
     </Text>
-    <Text style={{color: theme.colors.blue, fontFamily: 'Nunito-SemiBold'}}>
-      Calendrier
-    </Text>
-    <Text style={{color: theme.colors.blue, fontFamily: 'Nunito-SemiBold'}}>
-      Calendrier
-    </Text>
-    <View>
-      <Text style={{color: theme.colors.blue, fontFamily: 'Nunito-SemiBold'}}>
-        Calendrier
-      </Text>
-      <Text style={{color: theme.colors.blue, fontFamily: 'Nunito-SemiBold'}}>
-        Calendrier
-      </Text>
-      <Text style={{color: theme.colors.blue, fontFamily: 'Nunito-SemiBold'}}>
-        Calendrier
-      </Text>
-    </View>
-    <View style={styles.screen}>
-      <Text style={{color: theme.colors.blue, fontFamily: 'Nunito-SemiBold'}}>
-        Calendrier
-      </Text>
-      <Text style={{color: theme.colors.blue, fontFamily: 'Nunito-SemiBold'}}>
-        Calendrier
-      </Text>
-      <Text style={{color: theme.colors.blue, fontFamily: 'Nunito-SemiBold'}}>
-        Calendrier
-      </Text>
-    </View>
   </View>
 );
-
-const styles = StyleSheet.create({
-  screen: {
-    backgroundColor: '#F6F6F7',
-    width: '100%',
-    height: '100%',
-    flex: 6,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default Calendrier;
